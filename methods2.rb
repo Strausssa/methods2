33730eec70ef868f4f35981fdb442f8411ae2ea7
Sam Strauss <strausssa@s.dcsdk12.org>
2015-08-31 10:20:25 -0600

    tasks

--- methods2.rb
+++ methods2.rb
@@ -1,12 +1,62 @@
 module Methods2
 	
 	# TODO - write elevenish?
-	
+	def elevenish (num)
+		divided = num % 11
+		if divided == 1
+			return true
+		elsif divided ==0
+			return true
+		else
+			return false
+		end
+	end
+
 	# TODO - write ice_cream_party?
-	
+
+	def ice_cream_party (icecream,candy)
+		if icecream == 5 && candy == 5
+			return 0
+		elsif icecream <= 5 || candy <=5
+			return 1
+		else icecream > 5 || candy >=5
+			return 2
+		end
+	end
+
+
 	# TODO - write successful_squirrel_party?
-	
+	def successful_squirrel_party?(num, weekend)
+		if weekend == true
+			if num >= 40
+				true
+			else
+				false
+			end
+		else
+			if num >= 40 && num <= 60
+				true
+			else
+				false
+			end
+		end
+	end
+
+
 	# TODO - write ticket
+	def ticket(a, b, c)
+		ab = a + b
+		bc = b + c
+		ac = a + c
+
+		if ab == 10 || bc == 10 || ac == 10
+			return 10
+		elsif (ab - 10) == bc || (ab - 10) == ac
+			return 5
+		else
+			return 0
+		end
+	end
 
 	# TODO - write in_order?
 
--- methods2_test.rb
+++ methods2_test.rb
@@ -2,13 +2,39 @@ require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative 'methods'
+require_relative 'methods2'
 
 class Methods2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
-     include Methods
+     include Methods2
    	end.new
 	end
 
+  def test_elevenish
+    assert_equal true, @m.elevenish(11)
+    assert_equal true, @m.elevenish(12)
+    assert_equal false, @m.elevenish(10)
+  end
+
+  def test_ice_cream_party
+    assert_equal 0, @m.ice_cream_party(0,5)
+    assert_equal 1, @m.ice_cream_party(4,5)
+    assert_equal 1, @m.ice_cream_party(5,4)
+    assert_equal 2, @m.ice_cream_party(5,5)
+  end
+
+  def test_successful_squirrel_party
+    assert_equal false, @m.successful_squirrel_party?(80, false)
+    assert_equal false, @m.successful_squirrel_party?(34, true)
+    assert_equal true, @m.successful_squirrel_party?(100, true)
+    assert_equal true, @m.successful_squirrel_party?(54, false)
+  end
+
+  def test_ticket
+    assert_equal 5, @m.ticket(15, 4, 5)
+    assert_equal 0, @m.ticket(5, 4, 3)
+    assert_equal 10, @m.ticket(4, 6, 3)
+    assert_equal 10, @m.ticket(6, 4, 3)
+  end
 end
